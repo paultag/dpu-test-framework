930f0b728a4e26cb5b08a4a1c66060008d75b880
Niels Thykier <niels@thykier.net>
2012-08-16 12:14:03 +0200

    Add prototype manifest parser and tester
    Signed-off-by: Niels Thykier <niels@thykier.net>

---
+++ tests/test_manifest.py
@@ -0,0 +1,29 @@
+import os
+import tarfile
+
+from dpu.manifest import parse_manifest
+from dpu.util import disposabledir
+from dpu.tarball import open_compressed_tarball
+
+resources = "./tests/resources/"
+rundir= "./tests/staging/"
+
+def test_manifest():
+    testname = "manifest-tarball"
+    staging = os.path.join(rundir, testname)
+    test_res = os.path.join(resources, testname)
+    man = parse_manifest(os.path.join(test_res, "manifest"))
+    with disposabledir(staging):
+        tname = os.path.join(staging, "test.tar.gz")
+        tf = tarfile.open(tname, mode="w:gz")
+        tf.add(os.path.join(test_res, "root"), arcname=".", filter=__tar_filter)
+        tf.close()
+        with open_compressed_tarball(tname) as tar:
+            man.check_tarball(tar)
+
+def __tar_filter(tarinfo):
+    tarinfo.uname = "root"
+    tarinfo.gname = "root"
+    tarinfo.uid = 0
+    tarinfo.gid = 0
+    return tarinfo
