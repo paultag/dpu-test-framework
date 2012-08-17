22a5d3c92a6e3b6e6d6ae5a104a85188271507a2
Paul Tagliamonte <paultag@sunlightfoundation.com>
2012-08-16 22:40:46 -0400

    Adding in some docstrings

--- dpu/templates.py
+++ dpu/templates.py
@@ -1,18 +1,53 @@
-from dpu.utils import rsync, tmpdir, dir_walk, rm, cd
+# Copyright (c) DPU maintainers under the terms and conditions of the GPL-2+
+# license.
+"""
+This module contains Template classes to aid with rendering out model files
+into a test source directory.
+"""
+
+from dpu.utils import rsync, tmpdir, dir_walk, rm, cd, abspath
 from jinja2 import Template
 import os
 
 
 class PlainTemplate(object):
+    """
+    PlainTemplate classes manage the rendering of a model directory of all
+    "real" files to a secondary directory. This is the most basic Template
+    class there is.
+    """
+
     def __init__(self, where):
-        self._template_path = where
+        """
+        `where' is a path (abs or rel) to the Template directory, to be rendered
+        when one invokes `render()' on this class.
+        """
+        self._template_path = abspath(where)
 
     def render(self, dest):
+        """
+        `dest' is a path (abs or rel) to the output directory, or where to write
+        the model files to.
+        """
+        dest = abspath(dest)
         rsync(self._template_path, dest)
 
 
 class JinjaTemplate(PlainTemplate):
+    """
+    JinjaTemplate is a class to manage writing a model directory full of
+    Jinja2 template files (marked with a .tpl extention) out to the output
+    directory.
+    """
+
     def render(self, dest):
+        """
+        `dest' is a path (abs or rel) to the output directory, or where to write
+        the model files to. Be extra-sure to call setContext on this particular
+        class, it's important to render out the Jinja context correctly.
+        """
+        dest = abspath(dest)
+
         with tmpdir() as tmp:
             PlainTemplate.render(self, tmp)
             with cd(tmp):
@@ -26,16 +61,52 @@ class JinjaTemplate(PlainTemplate):
 
 
     def setContext(self, context):
+        """
+        This sets the context (just treat it as a dict) to be used during the
+        Jinja2 rendering process.
+        """
         self.context = context
 
 
 class UpstreamShim(PlainTemplate):
+    """
+    This "fake" Template is actually a hook into the Template rendering process
+    to allow us to save the upstream tar.gz before we taint it with "Debian"
+    stuff. This allows for non-native emulation.
+    """
+
+    def __init__(self):
+        """
+        OK, we're overloading this because we don't need a model directory.
+        """
+        pass  # Overload, we don't need an arg for a shim
+
     def render(self, dest):
+        """
+        We're actually rendering a tarball out, not files from a model
+        directory. We also expect that `dest' is in pkgname-verson format,
+        ready for taring up.
+        """
         pass  # Tar up the dest to ../
 
 
 class DebianShim(PlainTemplate):
+    """
+    This "fake" Template is actually a hook to clean any upstream Debian
+    directories out, before laying down some sanity.
+    """
+
+    def __init__(self):
+        """
+        OK, we're overloading this because we don't need a model directory.
+        """
+        pass  # Overload, we don't need an arg for a shim
+
     def render(self, dest):
+        """
+        We're actually just going to remove "%s/debian" % (dest), if it exists,
+        to make sure we don't have upstream crap futzing with us.
+        """
         pass  # Remove any ./debian directory
 
 
